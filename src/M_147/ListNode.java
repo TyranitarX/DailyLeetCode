c9845a40d04b779215fa2c6862209698cd84c910
TyranitarX <tyranitarx@163.com>
2020-11-20 10:25:51 +0800

    2020.11.20

---
+++ src/M_147/ListNode.java
@@ -0,0 +1,10 @@
+package M_147;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/M_147/M147_insertionSortList.java
@@ -0,0 +1,39 @@
+package M_147;
+
+public class M147_insertionSortList {
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode lastSorted = head, curr = head.next;
+        while (curr != null) {
+            if (lastSorted.val <= curr.val) {
+                lastSorted = lastSorted.next;
+            } else {
+                ListNode prev = dummyHead;
+                while (prev.next.val <= curr.val) {
+                    prev = prev.next;
+                }
+                lastSorted.next = curr.next;
+                curr.next = prev.next;
+                prev.next = curr;
+            }
+            curr = lastSorted.next;
+        }
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(3);
+        head = new M147_insertionSortList().insertionSortList(head);
+        System.out.println(head.val);
+        System.out.println(head.next.val);
+        System.out.println(head.next.next.val);
+        System.out.println(head.next.next.next.val);
+    }
+}
