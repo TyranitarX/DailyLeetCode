065cd5087fe75cadbcf6e2a2b312318d0afec430
TyranitarX <tyranitarx@163.com>
2020-11-30 10:48:34 +0800

    2020.11.30

--- src/M767_reorganizeString.java
+++ src/M767_reorganizeString.java
@@ -3,14 +3,12 @@ import java.util.*;
 public class M767_reorganizeString {
     public String reorganizeString(String S) {
         int[] counts = new int[26];
-        char countmax = 'a';
         int max = 0;
         for (int i = 0; i < S.length(); i++)
             counts[S.charAt(i) - 'a']++;
         for (int i = 0; i < S.length(); i++) {
             int count = counts[S.charAt(i) - 'a'];
             if (count > max) {
-                countmax = S.charAt(i);
                 max = count;
             }
         }
@@ -20,14 +18,14 @@ public class M767_reorganizeString {
         int evenIndex = 0, oddIndex = 1;
         int halfLength = S.length() / 2;
         for (int i = 0; i < 26; i++) {
-            char c = (char) ('a' + i);
+            char now = (char) (i + 'a');
             while (counts[i] > 0 && counts[i] <= halfLength && oddIndex < S.length()) {
-                reorganizeArray[oddIndex] = c;
-                counts[i]--;
+                reorganizeArray[oddIndex] = now;
                 oddIndex += 2;
+                counts[i]--;
             }
-            while (counts[i] > 0) {
-                reorganizeArray[evenIndex] = c;
+            while (counts[i] > 0 && evenIndex < S.length()) {
+                reorganizeArray[evenIndex] = now;
                 counts[i]--;
                 evenIndex += 2;
             }
@@ -36,6 +34,6 @@ public class M767_reorganizeString {
     }
 
     public static void main(String[] args) {
-        System.out.println(new M767_reorganizeString().reorganizeString("wawwivhwfrgontvvfggh"));
+        System.out.println(new M767_reorganizeString().reorganizeString("vvvlo"));
     }
 }
